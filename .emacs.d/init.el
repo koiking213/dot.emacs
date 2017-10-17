d8677bbb4df1332cdc814499668c050485f585aa
Fujii Yuya <fujii.yuya@jp.fujitsu.com>
2017-10-17 15:38:34 +0900

    migemoのLinux設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -67,20 +67,22 @@
 ;; use package
 (require 'use-package)
 
-;; ;;;===============================================
-;; ;;; cmigemo設定
-;; ;;;===============================================
-;; (use-package migemo)
-;; (when (eq system-type 'windows-nt)
-;;   (setq migemo-dictionary "C:/Users/koiki/AppData/Roaming/.emacs.d/conf/migemo/dict/cp932/migemo-dict")
-;;   (setq migemo-coding-system 'cp932-unix))
-;; (when (eq system-type 'gnu/linux)
-;;   (setq migemo-dictionary "C:/Users/koiki/AppData/Roaming/.emacs.d/conf/migemo/dict/utf-8/migemo-dict")
-;;   (setq migemo-coding-system 'utf-8-unix))
-;; (setq migemo-command "cmigemo")
-;; (setq migemo-options '("-q" "--emacs" "-i" "\a")) (setq migemo-user-dictionary nil) (setq migemo-regex-dictionary nil)
-;; (load-library "migemo") ; ロードパス指定.
-;; (migemo-init)
+;;;===============================================
+;;; cmigemo設定
+;;;===============================================
+(use-package migemo)
+(when (eq system-type 'windows-nt)
+  (setq migemo-dictionary "C:/Users/koiki/AppData/Roaming/.emacs.d/conf/migemo/dict/cp932/migemo-dict")
+  (setq migemo-coding-system 'cp932-unix))
+(when (eq system-type 'gnu/linux)
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-coding-system 'utf-8-unix))
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs" "-i" "\a"))
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(load-library "migemo") ; ロードパス指定.
+(migemo-init)
 
 ;; helm
 (bind-keys*
